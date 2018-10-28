b391861a1aa76fa18195f2d91492e05d3fa5452a
jitendar14 <jittug@gmail.com>
2018-10-28 19:57:13 +0000

    space added

--- OrangeHRMPageobject/src/test/java/OrangeHRMPageobject/OrangeHRMPageobject/SanityTestSuite.java
+++ OrangeHRMPageobject/src/test/java/OrangeHRMPageobject/OrangeHRMPageobject/SanityTestSuite.java
@@ -3,6 +3,7 @@ package OrangeHRMPageobject.OrangeHRMPageobject;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
